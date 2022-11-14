e9ea7dc1693f65755e569d43d68ac2a57b8611e7
Dev-Zaius <martin.paquet@univ-tlse3.fr>
2022-11-14 18:41:08 +0100

    fini les traitres

--- src/personnages/Traitre.java
+++ src/personnages/Traitre.java
@@ -1,4 +1,5 @@
 package personnages;
+import java.util.Random;
 
 public class Traitre extends Samourai{
 	private int niveauTraitrise;
@@ -9,22 +10,41 @@ public class Traitre extends Samourai{
 	}
 	
 	public void ranconner(Commercant commercant) {
-		int argentCommercant = commercant.getArgent();
-		int argentRanconner = (argentCommercant * 2 ) / 10;
-		commercant.perdreArgent(argentRanconner);
-		gagnerArgent(argentRanconner);
-		parler("Si tu veux ma protection contre les Yakuzas, il va falloir payer ! Donne-moi"
-				+ argentRanconner + " sous ou gare à toi !");
-		parler("Tout de suite grand" + getNom() + ".");
+		if (niveauTraitrise < 3) {
+			int argentCommercant = commercant.getArgent();
+			int argentRanconner = (argentCommercant * 2 ) / 10;
+			commercant.perdreArgent(argentRanconner);
+			gagnerArgent(argentRanconner);
+			parler("Si tu veux ma protection contre les Yakuzas, il va falloir payer ! Donne-moi "
+					+ argentRanconner +  " sous ou gare à toi !");
+			parler("Tout de suite grand " + getNom() + ".");
+			niveauTraitrise++;
+		} else {
+			parler("Mince je ne peux plus rançonner personne sinon un samouraï risque de me démasquer !");
+		}
 	}
 	
 	public void faireLeGentil() {
-		
+		if (nbConnaissance < 1) {
+			parler("Je ne peux faire ami ami avec personne car je ne connais personne ! Sif.");
+		} else {
+			int don = argent / 20;
+			Random rand = new Random();
+			Humain ami = memoire[rand.nextInt(nbConnaissance)];
+			String nomAmi = ami.getNom();
+			parler("Il faut absolument remonter ma cote de confiance. Je vais me faire ami ami avec "+ nomAmi +".");
+			parler("Bonjour l'ami ! Je voudrais vous aider en vous donnant " + don + " sous.");
+			ami.gagnerArgent(don);
+			perdreArgent(don);
+			ami.parler("Merci " + this.getNom() + ". Vous êtes quelqu'un de bien.");
+			if (niveauTraitrise > 1)
+				niveauTraitrise--;
+		}
 	}
 	
 	@Override
 	public void direBonjour() {
 		super.direBonjour();
-		parler("Mon niveau de traîtrise est de " + niveauTraitrise);
+		parler("Mais je suis un traître et mon niveau de traîtrise est : " + niveauTraitrise + ". Chut !");
 	}
 }
--- src/scenario/HistoireTP5.java
+++ src/scenario/HistoireTP5.java
@@ -22,6 +22,18 @@ public class HistoireTP5 {
 		System.out.println("");
 		akimoto.faireConnaissanceAvec(marco);
 		akimoto.boire("thé");
+
+		System.out.println("");
+		Traitre masako = new Traitre("Miyamoto", "Masako", "whisky", 100);
+		masako.faireLeGentil();
+		masako.ranconner(kumi);
+		masako.ranconner(chonin);
+		masako.ranconner(marco);
+		akimoto.faireConnaissanceAvec(masako);
+		masako.ranconner(kumi);
+		masako.faireConnaissanceAvec(yaku);
+		masako.faireLeGentil();
+		masako.faireConnaissanceAvec(roro);
 	}
 
 }
